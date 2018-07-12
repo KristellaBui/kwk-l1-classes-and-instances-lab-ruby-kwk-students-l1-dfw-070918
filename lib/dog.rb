6c54bc6ab1483981da20d8de35698d57b23096b1
KristellaBui <kristellabui@gmail.com>
2018-07-12 21:08:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,6 @@
 class Dog
 end
 
-def dogs (dog1, dog2, dog3)
-  @dog1 = dog1
-  @dog2 = dog2
-  @dog3 = dog3
-end 
-
-def dog1
-  @dog1
-end
-
-def dog2
-  @dog2
-end
-
-def dog3
-  @dog3
-end
-
-def update_dog1(Fido)
-    @dog1 = Fido
-  end 
+fido = dog.new
+snoopy = dog.new
+lassie = dog.new
